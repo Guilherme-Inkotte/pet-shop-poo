85828672dc2c0c0a9b122fe20e67fcd801b75ecc
Guilherme Inkotte <gui.inkotte@hotmail.com>
2021-11-05 22:19:47 -0300

    Adicionado método Main

---
+++ pet-shop-poo/src/module-info.java
@@ -0,0 +1,3 @@
+module petshop {
+	exports petshop;
+}
---
+++ pet-shop-poo/src/petshop/Main.java
@@ -0,0 +1,13 @@
+// Autores:
+// Gabriel Marchioro Klein, Guilherme Henrique Schneider Inkotte e Rodrigo Alves Bolincenha
+
+package petshop;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
